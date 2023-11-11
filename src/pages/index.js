f9702d12a4e544b965671ea9708bcb62f847bc2e
Сергей Ашарин <kalliak2014@yandex.ru>
2023-11-11 17:29:59 +0300

    basic setup for app working + some kind of 3d models

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import Projects from './Projects.jsx'
+
+export {Home, About, Contact, Projects};
